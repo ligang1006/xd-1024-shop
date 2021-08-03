2028a65ddd21a6355e0b2a41bfb3d3f2543d9d9a
ligang <ligangag@yonyou.com>
2021-08-03 17:42:52 +0800

    7.3微服务项目整合SwaggerUI3.0接口文档分组和Header头定义

--- readme.md
+++ readme.md
@@ -2,8 +2,8 @@
 
 
 
-###7
-* SwaggerUI3.0
+### 7 SwaggerUI3.0
+* SwaggerUI3.0的使用
     * 进行配置
     * 编写controller
     * 进行测试
@@ -69,3 +69,95 @@ public class AddressController {
 AddressController配置接口文档
 
 访问地址 http://localhost:9001/swagger-ui/index.html#/
+* SwaggerUI3.0接口文档分组
+  * 具体实现见common包下的config
+  * 增加一个Bean 同时修改group组的名称
+
+* SwaggerUI3.0接Header头定义
+  * 请求需要自定义http头，比如登录token令牌
+  * //新版swagger3.0配置
+    .globalRequestParameters(getGlobalRequestParameters())
+    .globalResponses(HttpMethod.GET, getGlobalResponseMessage())
+    .globalResponses(HttpMethod.POST, getGlobalResponseMessage());
+```
+
+import lombok.Data;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Component;
+import springfox.documentation.builders.*;
+import springfox.documentation.oas.annotations.EnableOpenApi;
+import springfox.documentation.schema.ScalarType;
+import springfox.documentation.service.*;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Bean
+    public Docket webApiDoc(){
+
+        return new Docket(DocumentationType.OAS_30)
+                .groupName("用户端接口文档")
+                .pathMapping("/")
+                // 定义是否开启swagger，false为关闭，可以通过变量控制，线上关闭
+                .enable(true)
+                //配置api文档元信息
+                .apiInfo(apiInfo())
+                // 选择哪些接口作为swagger的doc发布
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("net.xdclass"))
+                //正则匹配请求路径，并分配至当前分组
+                .paths(PathSelectors.ant("/api/**"))
+                //正则匹配请求路径，并分配至当前分组，当前所有接口
+                .paths(PathSelectors.any())
+
+                .build()
+
+                //新版swagger3.0配置
+                .globalRequestParameters(getGlobalRequestParameters())
+                .globalResponses(HttpMethod.GET, getGlobalResponseMessage())
+                .globalResponses(HttpMethod.POST, getGlobalResponseMessage());
+
+    }
+    
+   
+   /**
+     * 生成全局通用参数, 支持配置多个响应参数
+     * @return
+     */
+    private List<RequestParameter> getGlobalRequestParameters() {
+        List<RequestParameter> parameters = new ArrayList<>();
+        parameters.add(new RequestParameterBuilder()
+                .name("token")
+                .description("登录令牌")
+                .in(ParameterType.HEADER)
+                .query(q -> q.model(m -> m.scalarModel(ScalarType.STRING)))
+                .required(false)
+                .build());
+
+//        parameters.add(new RequestParameterBuilder()
+//                .name("version")
+//                .description("版本号")
+//                .required(true)
+//                .in(ParameterType.HEADER)
+//                .query(q -> q.model(m -> m.scalarModel(ScalarType.STRING)))
+//                .required(false)
+//                .build());
+        return parameters;
+    }
+
+    /**
+     * 生成通用响应信息
+     * @return
+     */
+    private List<Response> getGlobalResponseMessage() {
+        List<Response> responseList = new ArrayList<>();
+        responseList.add(new ResponseBuilder().code("4xx").description("请求错误，根据code和msg检查").build());
+        return responseList;
+    }
+}
+
+springfox.documentation.service.response
+```
--- xdclass-common/src/main/java/net/gaven/config/SwaggerUiConfiguration.java
+++ xdclass-common/src/main/java/net/gaven/config/SwaggerUiConfiguration.java
@@ -1,17 +1,18 @@
 package net.gaven.config;
 
 import lombok.Data;
-
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.*;
 import springfox.documentation.oas.annotations.EnableOpenApi;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.schema.ScalarType;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author: lee
@@ -22,7 +23,7 @@ import springfox.documentation.spring.web.plugins.Docket;
 @EnableOpenApi
 public class SwaggerUiConfiguration {
     @Bean
-    public Docket webApiDoc(){
+    public Docket webApiDoc() {
         return new Docket(DocumentationType.OAS_30)
                 .groupName("用户端接口文档")
                 .pathMapping("/")
@@ -35,8 +36,76 @@ public class SwaggerUiConfiguration {
                 .apis(RequestHandlerSelectors.basePackage("net.gaven"))
                 //正则匹配请求路径，并分配至当前分组
                 .paths(PathSelectors.ant("/api/**"))
-                .build();
+                .build()
+                //新版swagger3.0配置 配置全局的信息
+                .globalRequestParameters(getGlobalRequestParameters())
+                .globalResponses(HttpMethod.GET, getGlobalResponseMessage())
+                .globalResponses(HttpMethod.POST, getGlobalResponseMessage());
     }
+
+    @Bean
+    public Docket adminWebApiDoc() {
+        return new Docket(DocumentationType.OAS_30)
+                .groupName("管理端接口文档")
+                .pathMapping("/")
+                // 定义是否开启swagger，false为关闭，可以通过变量控制，线上关闭
+                .enable(true)
+                //配置api文档元信息
+                .apiInfo(apiInfo())
+                // 选择哪些接口作为swagger的doc发布
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("net.gaven"))
+                //正则匹配请求路径，并分配至当前分组
+                .paths(PathSelectors.ant("/admin/api/**"))
+
+                .build()
+                //新版swagger3.0配置 配置全局的信息
+                .globalRequestParameters(getGlobalRequestParameters())
+                .globalResponses(HttpMethod.GET, getGlobalResponseMessage())
+                .globalResponses(HttpMethod.POST, getGlobalResponseMessage());
+    }
+
+    /**
+     * 生成全局通用参数, 支持配置多个响应参数
+     *
+     * @return
+     */
+    private List<RequestParameter> getGlobalRequestParameters() {
+        List<RequestParameter> parameters = new ArrayList<>();
+        parameters.add(new RequestParameterBuilder()
+                .name("token")
+                .description("登录令牌")
+                .in(ParameterType.HEADER)
+                .query(q -> q.model(m -> m.scalarModel(ScalarType.STRING)))
+                .required(false)
+                .build());
+//添加其他的参数
+//        parameters.add(new RequestParameterBuilder()
+//                .name("version")
+//                .description("版本号")
+//                .required(true)
+//                .in(ParameterType.HEADER)
+//                .query(q -> q.model(m -> m.scalarModel(ScalarType.STRING)))
+//                .required(false)
+//                .build());
+        return parameters;
+    }
+
+    /**
+     * 全局的信息
+     * 生成通用响应信息
+     *
+     * @return
+     */
+    private List<Response> getGlobalResponseMessage() {
+        List<Response> responseList = new ArrayList<>();
+        responseList.add(new ResponseBuilder()
+                .code("4xx")
+                .description("请求错误，根据code和msg检查")
+                .build());
+        return responseList;
+    }
+
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
