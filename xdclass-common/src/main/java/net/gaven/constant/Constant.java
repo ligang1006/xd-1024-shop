d9c8f1f9cf8ea8dd202f7f980439ec4214abba60
ligang <ligangag@yonyou.com>
2021-08-05 14:17:06 +0800

    check captcha and send random num to e-mail

--- xdclass-common/src/main/java/net/gaven/constant/Constant.java
+++ xdclass-common/src/main/java/net/gaven/constant/Constant.java
@@ -7,4 +7,13 @@ package net.gaven.constant;
 public class Constant {
 
     public static final String REDIS_USER_SERVICE_PREFIX = "user-service:captcha";
+
+    /**
+     * 邮箱的主题
+     */
+    public static final String E_MAIL_SUBJECT = "邮箱验证码";
+    /**
+     * 邮箱的内容
+     */
+    public static final String E_MAIL_CONTENT = "欢迎来到召唤师峡谷";
 }
--- xdclass-user-service/src/main/java/net/gaven/service/impl/NotifyServiceImpl.java
+++ xdclass-user-service/src/main/java/net/gaven/service/impl/NotifyServiceImpl.java
@@ -1,9 +1,17 @@
 package net.gaven.service.impl;
 
+import net.gaven.enums.BizCodeEnum;
 import net.gaven.enums.SendCodeEnum;
+import net.gaven.service.IMailService;
 import net.gaven.service.INotifyService;
+import net.gaven.util.CheckUtil;
 import net.gaven.util.JsonData;
+import net.gaven.util.RandomNumUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import static net.gaven.constant.Constant.E_MAIL_CONTENT;
+import static net.gaven.constant.Constant.E_MAIL_SUBJECT;
 
 /**
  * @author: lee
@@ -12,10 +20,21 @@ import org.springframework.stereotype.Service;
 @Service
 public class NotifyServiceImpl implements INotifyService {
 
+    @Autowired
+    private IMailService mailService;
+
     @Override
     public JsonData sendCode(SendCodeEnum sendCodeEnum, String to) {
+        //发邮件
+        if (CheckUtil.isEmail(to)) {
+            String randomNum = RandomNumUtil.getRandomNum(6);
+            //发送随机数作为验证码
+            mailService.sendMsg(to, E_MAIL_SUBJECT, E_MAIL_CONTENT + randomNum);
+//            mailService.sendMsg(to, E_MAIL_SUBJECT, String.format(E_MAIL_CONTENT, randomNum));
+            return JsonData.buildSuccess("send e-mail success!");
+        } else if (CheckUtil.isPhone(to)) {
 
-        if ()
-        return null;
+        }
+        return JsonData.buildResult(BizCodeEnum.CODE_TO_ERROR);
     }
 }
