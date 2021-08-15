7c0229d3d87dfdd30d7f831e702af0d51df7a635
lee <ligangag@yonyou.com>
2021-08-15 14:14:14 +0800

    change get coupon by redission

--- xdclass-coupon-service/src/main/java/net/gaven/service/impl/RedissionCouponServiceImpl.java
+++ xdclass-coupon-service/src/main/java/net/gaven/service/impl/RedissionCouponServiceImpl.java
@@ -18,9 +18,13 @@ import net.gaven.model.LoginUser;
 import net.gaven.service.ICouponService;
 import net.gaven.util.JsonData;
 import net.gaven.util.MyRedisTemplate;
+import net.gaven.util.MyRedissionClient;
 import net.gaven.util.RandomUtil;
 import net.gaven.vo.CouponVO;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.core.script.DefaultRedisScript;
@@ -50,6 +54,8 @@ public class RedissionCouponServiceImpl implements ICouponService {
     private CouponRecordMapper couponRecordMapper;
     @Resource
     private MyRedisTemplate myRedisTemplate;
+    @Autowired
+    private RedissonClient redissioClient;
 
     @Override
     public Map<String, Object> pageCouponActivity(Integer page, Integer size) {
@@ -92,74 +98,22 @@ public class RedissionCouponServiceImpl implements ICouponService {
     @Transactional(rollbackFor = Exception.class)
     public JsonData addCoupon(Long couponId, CouponCategoryEnum category) {
         LoginUser loginUser = LoginInterceptor.threadLocal.get();
-
-
-        /**
-         * 通过原生的方法lua脚本实现redis锁
-         * 1、设置key
-         * 2、解锁
-         */
-        String key = "coupon:" + couponId;
-        String value = RandomUtil.getStringNumRandom(10);
-
-        Boolean lockFlag = myRedisTemplate.setIfAbsent(key, value, 10, TimeUnit.MINUTES);
-        //加锁成功
-        if (lockFlag) {
-            try {
-                log.info("add lock success key:{} value:{}", key, value);
-
-                //扣费逻辑
-                getCoupon(couponId, category, loginUser);
-            } finally {
-
-                //通过lua脚本实现原子性操作
-                String script = "if redis.call('get',KEYS[1]) == ARGV[1] " +
-                        "then return redis.call('del',KEYS[1]) else return 0 end";
-                //key为KEYS[1],value为传入的value ARGV[1] Integer.class lua脚本返回值类型
-                Long result = (Long) myRedisTemplate.luaKey(new DefaultRedisScript<Long>(script, Long.class), Arrays.asList(key), value);
-                log.info("解锁：{}", result);
-            }
-        } else {
-            //加锁失败，自旋
-            log.error("lock fail get lock again");
-            try {
-                TimeUnit.SECONDS.sleep(5);
-                addCoupon(couponId, category);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+        String lockKey = "coupon:" + couponId;
+        RLock lock = null;
+        try {
+            //加锁成功,其他线程进入会阻塞等待
+            lock = redissioClient.getLock(lockKey);
+            //有看门狗，默认30s 每10s钟会检查一下是否过期，如果快过期了会自动续期
+            lock.lock();
+            //没有watch dog
+//            lock.lock(10,TimeUnit.SECONDS);
+            log.info("add lock success key:{} threadId:{}", lockKey, Thread.currentThread().getId());
+            //扣费逻辑
+            getCoupon(couponId, category, loginUser);
+        } finally {
+            lock.unlock();
+            log.info("成功解锁 当前线程Id:{}", Thread.currentThread().getId());
         }
-
-//
-//        //校验校验优惠卷是否存在
-//        CouponDO couponDO = couponMapper.selectOne(new QueryWrapper<CouponDO>()
-//                .eq("id", couponId)
-//                .eq("category", category));
-//
-//        //校验优惠卷是否能够领取，时间、库存、类型
-//        if (loginUser == null) {
-//            loginUser = new LoginUser();
-//            loginUser.setId(Long.getLong(RandomUtil.getRandomNum(2)));
-//        }
-//        checkCoupon(couponDO, loginUser.getId());
-//
-//        CouponRecordDO couponRecordDO = new CouponRecordDO();
-//        BeanUtils.copyProperties(couponDO, couponRecordDO);
-//        couponRecordDO.setCreateTime(new Date());
-//        couponRecordDO.setUseState(CouponStateEnum.NEW.name());
-//        couponRecordDO.setUserId(loginUser.getId());
-//        couponRecordDO.setUserName(loginUser.getName());
-//        couponRecordDO.setCouponId(couponId);
-//        couponRecordDO.setId(null);
-//        //扣减优惠卷库存
-//        int reduceNum = couponMapper.reduceStock(couponId);
-////        int reduceNum = 1;
-//        //存库
-//        if (reduceNum == 1) {
-//            reduceCoupon(couponRecordDO);
-//        } else {
-//            log.error("reduce coupon fail couponId={},userId={}", couponId, loginUser.getId());
-//        }
         return JsonData.buildSuccess("get coupon success");
     }
 
