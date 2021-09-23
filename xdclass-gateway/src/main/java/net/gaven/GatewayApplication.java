261e84890e218c91d71259d2261336d39c6c36eb
lee <ligangag@yonyou.com>
2021-09-23 09:44:26 +0800

    add gateway

---
+++ xdclass-gateway/src/main/java/net/gaven/GatewayApplication.java
@@ -0,0 +1,17 @@
+package net.gaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author: lee
+ * @create: 2021/9/23 9:25 上午
+ **/
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
